aefca3066b7edcb8b7f9b47c621e91576563bcf9
fauzil17 <mutaqqimfauzil@gmail.com>
2026-02-21 11:31:18 +0800

    sdasd

--- includes/footer.php
+++ includes/footer.php
@@ -4,3 +4,15 @@
         <p>Jalan Trans Maumere-Larantuka | Telp: (021) 1234-5678 | Email: info@masjidkami.com</p>
     </div>
 </footer>
+
+<script src="assets/bootstrap/js/bootstrap.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.js.map"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js.map"></script>
+    <script src="" "assets/bootstrap/js/bootstrap-grip.js"></script>
+    <script src="" "assets/bootstrap/js/bootstrap-grip.js.map"></script>
+    <script src="" "assets/bootstrap/js/bootstrap-grip.min.js"></script>
+    <script src="" "assets/bootstrap/js/bootstrap-grip.min.js.map"></script>
+    <script src="" "assets/bootstrap/js/bootstrap-grip-rtl.js"></script>
+
+<script src="">
